90e971903b398de70544083eaea38b9bdf82d9ae
unknown <admin@valasku.com>
2019-11-01 03:59:26 +0700

    'http9'

---
+++ server.js
@@ -0,0 +1,47 @@
+
+var http = require('http');
+var express = require('express');
+var WSS = require('ws').Server;
+
+var app = express().use(express.static('public'));
+var server = http.createServer(app);
+
+const myport = process.env.PORT || 3000;
+
+server.listen(myport, '127.0.0.1');
+console.log(myport);
+var wss = new WSS({ port: myport });
+wss.on('connection', function(socket) {
+  console.log('Opened Connection 🎉');
+
+  var json = JSON.stringify({ message: 'Gotcha' });
+  socket.send(json);
+  console.log('Sent: ' + json);
+
+  socket.on('message', function(message) {
+    console.log('Received: ' + message);
+
+    wss.clients.forEach(function each(client) {
+      var json = JSON.stringify({ message: 'Something changed' });
+      client.send(json);
+      console.log('Sent: ' + json);
+    });
+  });
+
+  socket.on('close', function() {
+    console.log('Closed Connection 😱');
+  });
+
+});
+
+var broadcast = function() {
+  var json = JSON.stringify({
+    message: 'Hello hello!'
+  });
+
+  wss.clients.forEach(function each(client) {
+    client.send(json);
+    console.log('Sent: ' + json);
+  });
+}
+setInterval(broadcast, 3000);
