e3d0911e1b2afbc6e2f3aa7b530fe4545e1699ca
unknown <admin@valasku.com>
2019-11-01 15:24:36 +0700

    rev6

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 
-var myport = process.env.PORT || 80;
-var myhost =   "0.0.0.0";
+var myport = process.env.PORT || 3000;
+var myhost = "localhost";
+console.log(process.env);
+
 console.log("Port is : "+myhost+":"+myport);
+
 var http = require('http');
 var express = require('express');
 var WSS = require('ws').Server;
+var mysql = require('mysql');
+var request = require('request');
 
 var app = express().use(express.static('public'));
-var server = http.createServer(app);
+//app.listen(process.env.PORT || 3000);
+//var server = http.createServer(app);
 
-server.listen(myport,myhost);
+var server = http.createServer((app, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(myport ,function(){
+   console.log("up and running on port "+ "Port is : "+myhost+":"+myport);
+});
+return;
 
 
+//server.listen(process.env.PORT || 3000);
+//server.listen(port ,function(){
+ //   console.log("up and running on port "+ port);
+//});
 
+//server.listen(3000, '127.0.0.1');
+//server.listen(process.env.PORT || 3000);
+//server.listen(process.env.PORT, () => {});
 var wss = new WSS({ port: myport });
 wss.on('connection', function(socket) {
   console.log('Opened Connection 🎉');
@@ -25,6 +47,9 @@ wss.on('connection', function(socket) {
     console.log('Received: ' + message);
 
     wss.clients.forEach(function each(client) {
+
+       
+
       var json = JSON.stringify({ message: 'Something changed' });
       client.send(json);
       console.log('Sent: ' + json);
@@ -33,18 +58,51 @@ wss.on('connection', function(socket) {
 
   socket.on('close', function() {
     console.log('Closed Connection 😱');
+    
   });
 
 });
 
+var message="";
 var broadcast = function() {
-  var json = JSON.stringify({
-    message: 'Hello hello!'
-  });
+  var datetime = new Date();
+
 
   wss.clients.forEach(function each(client) {
+
+   
+    var queryString = 'SELECT * FROM market limit 1';
+    
+    /*con.query(queryString, function(err, rows, fields) {
+        if (err) throw err;
+    
+        for (var i in rows) {
+          message = rows[i].amount;
+            console.log('Query Result: ', rows[i].amount);
+        }
+        con.end();
+    });
+  */
+ 
+ request.get({
+    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+    url:     'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD',
+    //body:    "mes=heydude"
+  }, function(error, response, body){
+    console.log(body);
+    var response = JSON.parse(body);
+    message = response["USD"];
+  });
+
+    
+  var json = JSON.stringify({
+    message: message
+  });
+
     client.send(json);
     console.log('Sent: ' + json);
   });
+
+  
 }
-setInterval(broadcast, 3000);
+setInterval(broadcast, 1000);
